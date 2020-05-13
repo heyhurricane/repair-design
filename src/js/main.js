c6f0602ea22570bfca9be75f165f207b20855a40
heyhurricane <dinohka@mail.ru>
2020-05-13 17:51:25 +0400

    fix: js function for modal

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,28 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
+  var modalActive = false;
+
   const switchModal = () => { 
     modal.classList.toggle('modal--visible');
-  }
+    modalActive = !(modalActive);
+  };
+
   modalBtn.forEach(element => {
     element.addEventListener('click', switchModal);
   });
 
   closeBtn.addEventListener('click', switchModal);
+
+  document.addEventListener('click', (event) => {
+    if (event.target == modal) {
+      switchModal();
+    }
+  });
+
+  document.addEventListener('keydown', (event) => {
+    if (event.key=='Escape' && (modalActive)) {
+      switchModal();
+    }
+  });
 });
