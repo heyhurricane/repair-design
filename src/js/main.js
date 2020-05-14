62134f95f20c2e6edaf81eae1eeec0efc4b443da
heyhurricane <dinohka@mail.ru>
2020-05-14 14:13:38 +0400

    rewrite modal on jquery

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,57 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   const modal = document.querySelector('.modal');
+//   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//   const closeBtn = document.querySelector('.modal__close');
+//   
+
+//   const switchModal = () => { 
+//     modal.classList.toggle('modal--visible');
+//     modalActive = !(modalActive);
+//   };
+
+//   modalBtn.forEach(element => {
+//     element.addEventListener('click', switchModal);
+//   });
+
+//   closeBtn.addEventListener('click', switchModal);
+
+//   document.addEventListener('click', (event) => {
+//     if (event.target == modal) {
+//       switchModal();
+//     }
+//   });
+
+//   document.addEventListener('keydown', (event) => {
+//     if (event.key=='Escape' && (modalActive)) {
+//       switchModal();
+//     }
+//   });
+// });
+
+$(document).ready(function () {
   var modalActive = false;
-
-  const switchModal = () => { 
-    modal.classList.toggle('modal--visible');
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn =  $('.modal__close');
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
     modalActive = !(modalActive);
-  };
-
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
   });
-
-  closeBtn.addEventListener('click', switchModal);
-
-  document.addEventListener('click', (event) => {
-    if (event.target == modal) {
-      switchModal();
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+    modalActive = !(modalActive);
+  });
+  $(document).keydown(function(event) { 
+    if (event.key=='Escape' && (modalActive)) { 
+      modal.toggleClass('modal--visible');
+      modalActive = !(modalActive);
+    }
+  });
+  $(document).on('click', function(event) {
+    if ($(event.target).is(modal)) {
+      modal.toggleClass('modal--visible');
+      modalActive = !(modalActive);
     }
   });
 
-  document.addEventListener('keydown', (event) => {
-    if (event.key=='Escape' && (modalActive)) {
-      switchModal();
-    }
-  });
 });
