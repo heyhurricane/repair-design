39277ec1e01b0d3d9f8e05ce5e53c646349471a5
heyhurricane <dinohka@mail.ru>
2020-05-15 17:28:50 +0400

    add section Projects

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function(event) {
-//   const modal = document.querySelector('.modal');
-//   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-//   const closeBtn = document.querySelector('.modal__close');
-//   
-
-//   const switchModal = () => { 
-//     modal.classList.toggle('modal--visible');
-//     modalActive = !(modalActive);
-//   };
-
-//   modalBtn.forEach(element => {
-//     element.addEventListener('click', switchModal);
-//   });
-
-//   closeBtn.addEventListener('click', switchModal);
-
-//   document.addEventListener('click', (event) => {
-//     if (event.target == modal) {
-//       switchModal();
-//     }
-//   });
-
-//   document.addEventListener('keydown', (event) => {
-//     if (event.key=='Escape' && (modalActive)) {
-//       switchModal();
-//     }
-//   });
-// });
-
 $(document).ready(function () {
   var modalActive = false;
   var modal = $('.modal'),
@@ -53,22 +23,40 @@ $(document).ready(function () {
       modalActive = !(modalActive);
     }
   });
-  // $( "#target" ).keyup(function() {
-  //   alert( "Handler for .keyup() called." );
-  // });
-  $(document).ready(function(){   
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 0) {
-            $('.scroll-up').fadeIn();
-        } else {
-            $('.scroll-up').fadeOut();
-        }
-    });
-    $('.scroll-up').click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 900);
-        return false;
-    });
-});
+
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 0) {
+          $('.scroll-up').fadeIn();
+      } else {
+          $('.scroll-up').fadeOut();
+      }
+  });
+  $('.scroll-up').click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 900);
+      return false;
+  });
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  bullets.css('left', prev.width()+10);
+  next.css('left', prev.width()+10+bullets.width()+10);
+
+
 });
